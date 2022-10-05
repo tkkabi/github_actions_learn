1c91e409e729d4e483eca580c1ca8aa3416a9b2b
TAPAS KUMAR KABI <tapaskabinitr@gmail.com>
2022-10-05 07:43:44 +0100

    Create github-actions-script.sh

---
+++ github-actions-script.sh
@@ -0,0 +1,7 @@
+echo Add other actions to build,
+echo test, and deploy your project.
+ls -a
+python --version
+echo "---------------------------------"
+whoami
+echo "--------------------------------------------------------------------"
